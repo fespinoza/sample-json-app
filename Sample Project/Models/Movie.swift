d066d516c3fbeb3bedb996ec6d20755299f9a5bb
Felipe Espinoza <fespinozacast@gmail.com>
2024-02-04 20:09:26 +0100

    Add request to get a movie detail
    including the movie model.
    
    Interestingly, some fields are different between the `TrendingMovie` and `Movie`, that's the reason
    i'm not using the same model for both requests. Even its nested "ID container" is different

---
+++ Sample Project/Models/Movie.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct Movie: Decodable, Identifiable {
+    let ids: IDContainer
+    let title: String
+    let year: Int
+    let poster: String
+    let runtime: Int
+    let overview: String
+    let genres: [String]
+    let director: String
+    let ratings: Rating
+    let trailers: [Trailer]
+
+    var id: SimklMovieID { ids.simkl }
+    var imageURL: URL { ImageCdnURL.posterURL(for: poster) }
+}
+
+extension Movie {
+    struct IDContainer: Decodable {
+        let simkl: SimklMovieID
+        let slug: String
+    }
+
+    struct Rating: Decodable {
+        let simkl: SimklRating
+
+        struct SimklRating: Decodable {
+            let rating: Double
+            let votes: Int
+        }
+    }
+
+    struct Trailer: Decodable {
+        let name: String
+        let youtube: String
+        let size: Int
+    }
+}
--- Sample Project/Networking/SimklClient.swift
+++ Sample Project/Networking/SimklClient.swift
@@ -7,4 +7,11 @@ struct SimklClient {
         let (data, _) = try await urlSession.data(for: request)
         return try JSONDecoder().decode([TrendingMovie].self, from: data)
     }
+
+    func movie(with id: SimklMovieID) async throws -> Movie {
+        let request = GetMovieDetailsRequest(id: id).urlRequest
+        let urlSession = URLSession(configuration: .default)
+        let (data, _) = try await urlSession.data(for: request)
+        return try JSONDecoder().decode(Movie.self, from: data)
+    }
 }
