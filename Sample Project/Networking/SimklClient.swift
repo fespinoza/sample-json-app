5ff5b5add2c056b56df8fe83699e9647183c2f2c
Felipe Espinoza <fespinozacast@gmail.com>
2024-02-07 21:25:16 +0100

    Add networking for fetching the movie details given an ID
    including the movie model.
    
    Interestingly, some fields are different between the `TrendingMovie` and `Movie`, that's the reason
    i'm not using the same model for both requests. Even its nested "ID container" is different

--- Sample Project/Networking/SimklClient.swift
+++ Sample Project/Networking/SimklClient.swift
@@ -5,6 +5,12 @@ struct SimklClient {
         let request = GetTrendingMoviesWeekRequest().urlRequest
         return try await load(request)
     }
+
+    func movie(with id: SimklMovieID) async throws -> Movie {
+        let request = GetMovieDetailsRequest(id: id).urlRequest
+        return try await load(request)
+    }
+
     private func load<RequestData: Decodable>(_ request: URLRequest) async throws -> RequestData {
         let urlSession = URLSession(configuration: .default)
         let (data, _) = try await urlSession.data(for: request)
