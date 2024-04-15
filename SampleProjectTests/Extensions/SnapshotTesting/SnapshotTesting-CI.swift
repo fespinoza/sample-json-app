e847d5d3c3b3a0a57e8248bd6d7ecba2611733ad
Felipe Espinoza <fespinozacast@gmail.com>
2024-04-15 23:05:11 +0200

    Conditionally use the test bundle directory

--- SampleProjectTests/Extensions/SnapshotTesting/SnapshotTesting-CI.swift
+++ SampleProjectTests/Extensions/SnapshotTesting/SnapshotTesting-CI.swift
@@ -3,6 +3,20 @@ import SnapshotTesting
 import XCTest
 
 extension XCTestCase {
+    var isRunningOnCI: Bool {
+        ProcessInfo.processInfo.environment["CI"]?.lowercased() == "true"
+    }
+
+    /// Returns the URL where the bundle is present
+    ///
+    /// Used to specify the location of the snapshot references on CI
+    private var testBundleUrl: URL {
+        guard let url = Bundle(for: TestNamespace.self).resourceURL else {
+            fatalError("❌ we couldn't access to the test bundle URL")
+        }
+        return url
+    }
+
     public func customAssertSnapshot<Value, Format>(
         of value: @autoclosure () throws -> Value,
         as snapshotting: Snapshotting<Value, Format>,
@@ -17,9 +31,9 @@ extension XCTestCase {
             .deletingPathExtension()
             .lastPathComponent
 
-        let snapshotDirectory = TestNamespace.sourceCodeTestDirectory()
-            .appending(path: "__Snapshots__/\(testClassName)")
-            .path()
+        let baseDirectory = isRunningOnCI ? testBundleUrl : TestNamespace.sourceCodeTestDirectory()
+
+        let snapshotDirectory = baseDirectory.appending(path: "__Snapshots__/\(testClassName)").path()
 
         let failure = verifySnapshot(
             of: try value(),
